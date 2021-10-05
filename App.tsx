b8a6e9ddc831a5cd7371ffd4b55b9d3f039ecb58
Matthew Nascimento <matthewromie@hotmail.com>
2021-10-05 13:46:17 -0300

    estilização global

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold, useFonts
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+import { Dashboard } from './src/screens/Dashboard';
+
+
+
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
+  );
+}
+
+
